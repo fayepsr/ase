6f87cb54ce4e41f40e68decdd1ee3d5c1c4d7b0a
Foteini Psarra <Fotini2@live.com>
2022-03-24 15:18:55 +0200

    JAVA model runn

--- src/src_php/java_model.php
+++ src/src_php/java_model.php
@@ -1,10 +1,10 @@
 <?php 
 
-$command = escapeshellcmd('javac -d /home/src_java/ -cp /home/src_java/SHOracle.jar /home/src_java/Main.java');
-$output = shell_exec($command);
-echo $output;
+// $command = escapeshellcmd('javac -d /home/src_java/ -cp /home/src_java/SHOracle.jar /home/src_java/Main.java');
+// $output = shell_exec($command);
+// echo $output;
 
-$command = escapeshellcmd('java  -cp /home/src_java/SHOracle.jar  -classpath /home/src_java/ Main');
+$command = escapeshellcmd('java -cp /home/src_java/SHOracle.jar /home/src_java/Main.java');
 $output = shell_exec($command);
 echo $output;
 
