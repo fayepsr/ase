98cc3651195349e395569025157bfb6cd0863692
Foteini Psarra <Fotini2@live.com>
2022-04-09 12:02:29 +0300

    changed image names

--- src/build_and_run_all.bat
+++ src/build_and_run_all.bat
@@ -1,22 +1,17 @@
-
-::docker stop annotation_container
-
-::#docker rm annotation_container
-
 doker-compose down
 
-docker image rm charl/src_php:latest
-docker image rm charl/charl/src_learner:latest
-docker image rm charl/src_react:latest
+docker image rm src_php:latest
+docker image rm src_learner:latest
+docker image rm src_react:latest
 
 cd ./src_react
-docker build -f ./dockerfile -t charl/src_react:latest .
+docker build -f ./dockerfile -t src_react:latest .
 
 cd ../src_php
-docker build -f ./dockerfile -t charl/src_php:latest .
+docker build -f ./dockerfile -t src_php:latest .
 
 cd ../src_learner
-docker build -f ./dockerfile -t charl/src_learner:latest .
+docker build -f ./dockerfile -t src_learner:latest .
 
 cd ../
 
