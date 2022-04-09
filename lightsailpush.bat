cac78395d53b0fb1f2efb6f19f4dc2e098d6dd2d
Foteini Psarra <Fotini2@live.com>
2022-04-09 19:16:38 +0300

    lightsail push images

---
+++ lightsailpush.bat
@@ -0,0 +1,3 @@
+aws lightsail push-container-image --service-name ase-service-1 --label php --image src_php:latest
+aws lightsail push-container-image --service-name ase-service-1 --label react --image src_react:latest
+aws lightsail push-container-image --service-name ase-service-1 --label learner --image src_learner:latest
