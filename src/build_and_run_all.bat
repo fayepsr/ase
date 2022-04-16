284bbf09133dcfaad81874b5d560062f6fd0ade7
Foteini Psarra <Fotini2@live.com>
2022-04-16 14:34:25 +0300

    bug: fixed typo

--- src/build_and_run_all.bat
+++ src/build_and_run_all.bat
@@ -1,11 +1,12 @@
-doker-compose down
+
+docker-compose down
 
 docker image rm src_php:latest
 docker image rm src_learner:latest
 docker image rm src_react:latest
 
 cd ./src_react
-docker build -f ./dockerfile -t src_react:latest .
+docker build -f ./dockerfile -t src_react:latest . 
 
 cd ../src_php
 docker build -f ./dockerfile -t src_php:latest .
