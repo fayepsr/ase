bc967559c5f7809858c4ac5b50fc2b81183af857
Sukriti Sinha <sukriti.sinha@uzh.ch>
2022-04-27 17:31:08 +0200

    Some tests

--- src/src_learner/testing.py
+++ src/src_learner/testing.py
@@ -9,10 +9,22 @@ import highlight
 class Test(unittest.TestCase):
     
     def runTest(self):
+        #For Predict
         #Test for empty content
         self.assertEqual(highlight.predict('', 'python'), {'ok': 1, 'prediction': [], 'result': []}, "Unsuccessful run of function")
-        #Test with content
-        #self.assertEqual(highlight.predict('', 'python'), {'ok': 1, 'prediction': [8, 6, 7, 6, 11, [401 chars]41}]}, "Unuccessful run of function")
+        #Test with content (without testing what prediction returns)
+        self.assertDictContainsSubset( {'ok': 1}, highlight.predict('ZGVmIHR5cGluZyh3b3JkKToK', 'python'), "Unuccessful run of function")
+        #Test with language not in use
+        self.assertEqual(highlight.predict('', 'java'), {'ok': -1, 'msg': 'Not yet accepting this language'}, "Language error")
+
+        #For Finetune
+        #Test for empty content
+        self.assertEqual(highlight.finetune('', 'python'), {'ok': 1}, "Unsuccessful run of function")
+        #Test with content (without testing what prediction returns)
+        self.assertDictContainsSubset( {'ok': 1}, highlight.finetune('ZGVmIHR5cGluZyh3b3JkKToK', 'python'), "Unuccessful run of function")
+        #Test with language not in use
+        self.assertEqual(highlight.finetune('', 'java'), {'ok': -1, 'msg': 'Not yet accepting this language'}, "Language error")
+        
 
 
 #run the rest
