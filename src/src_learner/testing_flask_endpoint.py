6451d7b244b2461f946204cf4827f21114223bdd
Sukriti Sinha <sukriti.sinha@uzh.ch>
2022-05-01 13:11:42 +0200

    Merge branch 'error-logging'

---
+++ src/src_learner/testing_flask_endpoint.py
@@ -0,0 +1,26 @@
+import pytest
+import flask_endpoints
+
+
+@pytest.fixture()
+def app():
+    app = flask_endpoints()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
