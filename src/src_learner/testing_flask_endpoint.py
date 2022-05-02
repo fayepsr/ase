94f62512c0c249c0c27ed60c6442d30cb7406dfe
Sukriti Sinha <sukriti.sinha@uzh.ch>
2022-05-02 19:20:13 +0200

    flask testing

--- src/src_learner/testing_flask_endpoint.py
+++ src/src_learner/testing_flask_endpoint.py
@@ -4,6 +4,7 @@ import flask_endpoints
 
 @pytest.fixture()
 def app():
+    print("here")
     app = flask_endpoints()
     app.config.update({
         "TESTING": True,
@@ -16,11 +17,16 @@ def app():
     # clean up / reset resources here
 
 
-@pytest.fixture()
+def test_predict(client):
+    rv = client.post("/api_predict?t?code_to_format=cHJpbnQoKQ==&language=python")
+    assert b'{"ok": 1,"prediction": [9,0,0],"result": [{"startIndex": 0,"endIndex": 4,"lItemtokenId": 42},{"startIndex": 5,"endIndex": 5,"lItemtokenId": 54},{"startIndex": 6,"endIndex": 6,"lItemtokenId": 55}]}' == rv.data
+
+
+"""@pytest.fixture()
 def client(app):
     return app.test_client()
 
 
 @pytest.fixture()
 def runner(app):
-    return app.test_cli_runner()
+    return app.test_cli_runner()"""
