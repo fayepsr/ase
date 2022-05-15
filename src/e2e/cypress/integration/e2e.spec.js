681b387ee5e7cbbcb0645cace683c295f80de94c
ninmw <little.nina98@gmail.com>
2022-05-15 21:49:41 +0200

    Set up e2e testing

---
+++ src/e2e/cypress/integration/e2e.spec.js
@@ -0,0 +1,15 @@
+describe('ASE tests', () => {
+  it('loads successfully', () => {});
+
+  it('displays Java result in HTML format', () => {});
+  it('displays Java result in JSON format', () => {});
+  it('displays error on empty Java input', () => {});
+
+  it('displays Kotlin result in HTML format', () => {});
+  it('displays Kotlin result in JSON format', () => {});
+  it('displays error on empty Kotlin input', () => {});
+
+  it('displays Python result in HTML format', () => {});
+  it('displays Python result in JSON format', () => {});
+  it('displays error on empty Python input', () => {});
+});
